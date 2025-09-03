117a14eb3396d05c442ff2d2bb891868a09c7d0f
MonilDoshi <doshimonil91@gamil.com>
2025-09-04 00:35:30 +0530

    Add Heropage component and integrate Here_section_right; update Top_nav styles

--- vite-project/src/Homepage.jsx
+++ vite-project/src/Homepage.jsx
@@ -1,11 +1,12 @@
 import Leftside_nav from './component/Leftside_nav'
 import Top_nav from './component/Top_nav'
+import Heropage from './component/Heropage'
 
 
 function Homepage() {
 
     return (
-    <div className="flex h-screen w-screen overflow-hidden">
+    <div className="flex h-screen w-screen">
       {/* Sidebar */}
       <div className="fixed left-0 top-0 h-full w-20">
         <Leftside_nav />
@@ -18,6 +19,9 @@ function Homepage() {
           <Top_nav />
         </div>
 
+        <div className='flex-1 overflow-y-auto'>
+            <Heropage />
+        </div>
       </div>
     </div>
     )
---
+++ vite-project/src/component/Heropage.jsx
@@ -0,0 +1,11 @@
+import Here_section_right from "./Hero_section_right";
+
+function Heropage() {
+    return(
+
+        <Here_section_right />
+    )
+}
+
+
+export default Heropage
